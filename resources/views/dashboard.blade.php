2db78015f1273606bae4bd6fc74e781337644f14
fredtaran <ftaran04@gmail.com>
2023-09-05 17:09:34 +0800

    Admin dashboard in progress

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,27 @@
     <div class="py-2">
         <h1 class="text-2xl text-center font-bold">Appearance Logs</h1>
 
-        
+        <table id="appearance_log_table">
+            <thead>
+                <th>Serial Number</th>
+                <th>Fullname</th>
+                <th>Company</th>
+                <th>Inclusive Dates</th>
+                <th>Purpose</th>
+                <th>Date Issued</th>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
     </div>
 </section>
+@endsection
+
+@section('custom_script')
+<script>
+    $(document).ready(function() {
+        $('#appearance_log_table').DataTable();
+    });
+</script>
 @endsection
